e8d373be3a15a0a4e673324df3b03885730a671b
Lopè Ariyo <lopeariyo@gmail.com>
2019-10-31 19:23:39 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -18,7 +18,7 @@ def introduction_with_language(name,language)
 
 end
 
-def introduction_with_language_optional(name, language = Ruby)
+def introduction_with_language_optional(name, language)
 
 
   if language == nil
